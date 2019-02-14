6789cee77c2081c7dbe3074fcb95ee9ea9ec070d
David McClure <davidwilliammcclure@gmail.com>
2019-02-14 18:26:21 -0500

    Initial commit.

---
+++ ctx_attn/utils.py
@@ -0,0 +1,38 @@
+
+
+import sys
+import ujson
+import gzip
+
+from glob import glob
+
+
+def group_by_sizes(L, sizes):
+    """Given a flat list and a list of sizes that sum to the length of the
+    list, group the list into sublists with corresponding sizes.
+
+    Args:
+        L (list)
+        sizes (list<int>)
+
+    Returns: list<list>
+    """
+    parts = []
+
+    total = 0
+    for s in sizes:
+        parts.append(L[total:total+s])
+        total += s
+
+    return parts
+
+
+def read_json_gz_lines(root):
+    """Read JSON corpus.
+
+    Yields: dict
+    """
+    for path in glob('%s/*.gz' % root):
+        with gzip.open(path) as fh:
+            for line in fh:
+                yield ujson.loads(line)
